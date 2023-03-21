01c81e79269f4693f9b3dead7a65af483734ef12
GeorgeAntwanHosny <georgeantwanhosnyghaly@outlook.com>
2023-03-21 15:36:11 +0200

    add project files with booking API

--- BookingAPI/models.py
+++ BookingAPI/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 
 # Create your models here.
-class Book(models.Model):
-    title = models.CharField(max_length=255)
-    author = models.CharField(max_length=255)
-    price = models.DecimalField(max_digits=5, decimal_places=2)
+class Booking(models.Model):
+    Name = models.CharField(max_length=255)
+    No_of_guests = models.IntegerField(max_length=6)
+    BookingDate = models.DateTimeField()
--- BookingAPI/serializers.py
+++ BookingAPI/serializers.py
@@ -1,9 +1,9 @@
 from rest_framework import serializers
 
-from BookingAPI.models import Book
+from BookingAPI.models import Booking
 
 
 class BookSerializer(serializers.ModelSerializer):
     class Meta:
-        model = Book
+        model = Booking
         fields = ['id', 'title', 'author', 'price']
